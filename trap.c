6313ccdd55ff9a759cee46ad97c99e9780d46612
Soumaya <soumaya.ikhrichi5@etu.univ-lorraine.fr>
2022-01-03 22:10:34 +0100

    modification dans le trap.c fait avec Rawan

--- trap.c
+++ trap.c
@@ -11,17 +11,23 @@ trap_t* init_trap(trap_t trap1,trap_t trap2,trap_t trap3,trap_t trap4){
 
 void showTrap(trap_t trap,player_t* player,SDL_Event* event){
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* fenetre=SDL_CreateWindow("Trap",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,400,600,SDL_WINDOW_OPENGL);
-    SDL_Renderer* renderer=SDL_CreateRenderer(fenetre,-1,SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    TTF_Font* font;
-    init_ttf();
-    font=load_font("ressources/only.ttf",14);
+    SDL_Window *fenetre1=SDL_CreateWindow("Trap",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,400,600,SDL_WINDOW_OPENGL);
+    SDL_Renderer *renderer1=SDL_CreateRenderer(fenetre1,-1,0);
+    TTF_Init();
+    TTF_Font* font= TTF_OpenFont("ressources/arial.ttf",20);
+    SDL_Color color={255,255,255,255};
+    SDL_Surface* textSurf;
+    textSurf = TTF_RenderText_Solid(font,trap.question,color);
+    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer1,textSurf);
+    SDL_Rect textRect ={30,30,textSurf->w,textSurf->h};
+    SDL_FreeSurface(textSurf);
+    TTF_CloseFont(font);
     bool end=false;
     const char* reponse_j;
     const char* r;
-    SDL_SetRenderDrawColor(renderer,255,0,0,255);
+    //SDL_SetRenderDrawColor(renderer1,0,0,225,225);
     while(!end){
-        apply_text(renderer,0,0,100,200,"trap.question",font);
+      //  apply_text(renderer1,0,0,100,200,trap.question,font);
         while (SDL_PollEvent(event))
         {  
             if (event->type ==SDL_QUIT){
@@ -41,17 +47,26 @@ void showTrap(trap_t trap,player_t* player,SDL_Event* event){
                 }
                 else{
                     r= SDL_GetKeyName(event->key.keysym.sym);
+           
                     reponse_j +=*r;
-                    apply_text(renderer,0,201,100,200,reponse_j,font);
+                   textSurf = TTF_RenderText_Solid(font,reponse_j,color);
+               
+          
                 }
+                       
             }
+            
+                 
         }
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
+
+        SDL_RenderCopy(renderer1,textTexture,NULL, &textRect);
+        SDL_RenderPresent(renderer1);
+        SDL_UpdateWindowSurface(fenetre1);
     }
     // Quitter SDL
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(fenetre);
-    clean_font(font);
+    SDL_DestroyRenderer(renderer1);
+    SDL_DestroyWindow(fenetre1);
+
     SDL_Quit();
+    TTF_Quit();
 }
